944086e294130af6b06b37dfe037e322217fb6bc
alexxaviermalizacapuz-sys <alexxaviermalizacapuz@gmail.com>
2025-09-02 23:02:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ with st.sidebar:
         label='Porcentaje de concentración de hierro',
         min_value=61.00,
         max_value=69.00,
-        Value=65#Valor incial
+        Value=65.00,#Valor incial
         step=0.001,
         format="%.2f" 
     )
